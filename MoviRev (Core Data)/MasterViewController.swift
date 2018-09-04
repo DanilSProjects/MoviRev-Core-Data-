e8f70c3a9c612642af92e6ee39817d651c4861af
TwoEyedMikeWazowski <danielsyrov9@gmail.com>
2018-09-04 11:27:57 +0800

    Alphabetical order

--- MoviRev (Core Data)/MasterViewController.swift
+++ MoviRev (Core Data)/MasterViewController.swift
@@ -151,7 +151,7 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
         fetchRequest.fetchBatchSize = 20
         
         // Edit the sort key as appropriate.
-        let sortDescriptor = NSSortDescriptor(key: "timestamp", ascending: false)
+        let sortDescriptor = NSSortDescriptor(key: "name", ascending: true)
         
         fetchRequest.sortDescriptors = [sortDescriptor]
         
